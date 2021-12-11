9bf771b648363e3687b29f9a7641697ba2322ea0
Mickey <mdasilva@callutheran.edu>
2021-12-11 12:13:57 -0500

    Added item attribute lists

--- subfunctions/config.py
+++ subfunctions/config.py
@@ -36,5 +36,6 @@ standardHorse = {
             'Pace' : '20', 'Size' : '10', 'Wind' : '14/14'
         };
 weaponAttrs = ['Item', 'Class', 'Shots', 'Ammo', 'Caliber', 'RoF', 'Damage', 'Range Increment', 'DB', 'Price'];
-
+clothesAttrs = ['Item', 'Armour', 'Price'];
+ruckAttrs = ['Item', 'Quantity', 'Price per Unit']
 maxEandD = 5; #maximum number of edges and hinderances allowed. Determined by Marshal
