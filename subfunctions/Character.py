876edf01bf151db59317bcc4c0843a6f4269d2e0
Mickey <mdasilva@callutheran.edu>
2021-12-14 22:03:54 -0500

    Ancient whitespace

--- subfunctions/Character.py
+++ subfunctions/Character.py
@@ -1,18 +1,19 @@
 class Character:
-	def __init__( self, name = '', charClass = '', attrib = {}, subAttr = {}, \
-						wounds = [], chips = [], arcaneAbilities = [], EandD = {}, equipment = {}, charNotes = {}, gameNotes = {} ):
-		#Inherent
-		self.name = name;
-		self.charClass = charClass;
-		self.attrib = attrib;
-		self.subAttr = subAttr;
-		self.wounds = wounds;
-		self.chips = chips;
-		
-		self.arcaneAbilities = arcaneAbilities;
-		self.EandD = EandD;
-		
-		# Conditional
-		self.equip = equipment;
-		self.charNotes = charNotes;
-		self.gameNotes = gameNotes;
+    def __init__( self, name = '', charClass = '', attrib = {}, subAttr = {}, \
+                        wounds = [], chips = [], arcaneAbilities = [], edges = {}, hinders = {}, equipment = {}, charNotes = {}, gameNotes = {} ):
+        #Inherent
+        self.name = name;
+        self.charClass = charClass;
+        self.attrib = attrib;
+        self.subAttr = subAttr;
+        self.wounds = wounds;
+        self.chips = chips;
+        
+        self.arcaneAbilities = arcaneAbilities;
+        self.edges = edges;
+        self.hinds = hinders;
+        
+        # Conditional
+        self.equip = equipment;
+        self.charNotes = charNotes;
+        self.gameNotes = gameNotes;
