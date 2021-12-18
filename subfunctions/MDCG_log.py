abf138066ad2ebc636f599a7a298f822ae360090
Mickey <mdasilva@callutheran.edu>
2021-12-18 12:30:23 -0500

    Made schweet investigate function to help figure out object types, string representations, and member functions / values.

--- subfunctions/MDCG_log.py
+++ subfunctions/MDCG_log.py
@@ -1,9 +1,46 @@
 import inspect
 from datetime import datetime
+from math import floor
 
 global DEBUG_LEVEL
 from config import DEBUG_LEVEL
 
+def investigate( obj ):
+    objMeths = [ method_name for method_name in dir( obj ) if callable( getattr( obj, method_name ) ) ];
+    objVals  = [ val_name for val_name in dir( obj ) if not callable( getattr( obj, val_name ) ) ];
+    
+    print( 'Object: \t{}'.format( obj ) );
+    print( 'Str Rep: \t{}'.format( str( obj ) ) );
+    print( 'Type: \t\t{}'.format( type( obj ) ) );
+    print();
+    print( 'Methods:');
+    print_vals( objMeths, 4 );
+    print();
+    print( 'Values:');
+    print_vals( objVals, 4 );
+
+def print_vals( listObj, numCols ):
+    numRows = floor( len( listObj ) / numCols );
+    longestWord = get_longest_entry( listObj );
+    
+    sepList = [];
+    for iRow in range( numRows ):
+        sepList.append( listObj[iRow * numCols : iRow*numCols + numCols] );
+    if( (len( listObj ) / numCols) % numCols != 0 ):
+        sepList.append( listObj[numRows*numCols :] ); # append the rest
+    
+    for valList in sepList:
+        for item in valList:
+            print( '{}'.format( item ), end = '' );
+            for spaceBuff in range( longestWord - len( item ) ):
+                print(' ', end = '' );
+        print();
+def get_longest_entry( listObj ):
+    longestEntry = 0;
+    for item in listObj:
+        if( len(item) > longestEntry ):
+            longestEntry = len( item );
+    return longestEntry;
 def MDCG_log( strIn, logFile = ''):
 	#Print calling function information
 	lastFunc 		= inspect.stack()[2]; #assumes this is always being called from db_log
